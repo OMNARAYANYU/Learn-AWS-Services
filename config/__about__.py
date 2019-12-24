25be4a029d743d33e6c6f2dd6f3d3fce599738b2
Om Narayan <on371@nyu.edu>
2019-12-24 17:14:33 -0500

    adding the config files

---
+++ config/__about__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "aws-services-learning"
+__summary__ = ("Learn AWS important services from scratch")
+__uri__ = "https://github.com/OMNARAYANYU/Learn-AWS-Services.git"
+
+__version__ = "0.0.1"
+
+__author__ = "Om Narayan"
+
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "Copyright 2019 {0}".format(__author__)
